a60f34e0f44da99e2f5cc55e6f08bdb95dcc8a5f
Eric Frick <eric.frick@icloud.com>
2024-03-30 14:51:49 -0400

    Updated landing page

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,17 +1,39 @@
 import "@repo/ui/styles.css";
 import { Button } from "@repo/ui";
 
-export type AppProps = {
-  
-};
+import { useState } from 'react'
 
-const App = ({}: AppProps) => {
+const App = () => {
+  const [count, setCount] = useState(0);
+
   return (
-    <div>
-        <h1 className="text-blue-500 mb-6 text-5xl font-bold">App</h1>
-        <Button />
-    </div>
-  );
-};
+    <>
+     <div className="bg-gray-900 h-screen">
 
-export default App;
+     <div className="relative isolate px-6 pt-14 lg:px-8">
+        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+          <div className="text-center">
+            <h1 className="text-4xl font-bold tracking-tight text-gray-100 sm:text-6xl">
+              TurboRepo
+            </h1>
+            <div className="relative rounded-full px-3 py-1 text-lg leading-6 text-gray-300 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+              Vite + React + Tailwind
+            </div>
+            <div className="mt-10 flex items-center justify-center gap-x-6">
+              <Button onClick={() => setCount(prev => prev + 1)}>
+                Increment
+              </Button>
+              <Button onClick={() => setCount(prev => prev - 1)}>
+                Decrement
+              </Button>
+              <h4 className="text-sm font-semibold leading-6 text-gray-400">Count: {count}</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+     </div>
+    </>
+  )
+}
+
+export default App
--- packages/ui/src/components/Button.tsx
+++ packages/ui/src/components/Button.tsx
@@ -1,9 +1,11 @@
-export type ButtonProps = {
-  
-};
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  onClick: () => void;
+}
 
-export const Button = ({}: ButtonProps) => {
+export const Button = ({ onClick, children, ...props }: ButtonProps) => {
+  const baseStyle = "text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2";
+
   return (
-    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">UI Button</button>
-  );
+    <button onClick={onClick} {...props} className={baseStyle}>{children}</button>
+  )
 };
